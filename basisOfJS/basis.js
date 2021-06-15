d79b220f653804051dba8e160b571cf22fcf65a7
Aleksandr Kogutenko <oleksandr.kogutenko@gmail.com>
2021-06-15 23:38:01 +0300

    start

---
+++ basisOfJS/basis.js
@@ -0,0 +1,16 @@
+//******************************************************************* */
+//** 1 Variables */
+//******************************************************************* */
+
+var nameVar1 = 'Vladimer' // first case to create variable
+const nameVar2 = true // second case
+//let creates block scope local variable instead.
+let nameVar3 = 23         // third case
+
+/*
+   use "var" or "let" variables you CAN change inner container
+   use "const" variables you CANNOT change inner container
+*/
+nameVar1 = 'Vlad';
+console.log(nameVar1)
+
